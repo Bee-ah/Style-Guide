64abae8f2504e865f4530ce6a6ca1338d21077fd
Beatriz <beatrizleite_aragao@hotmail.com>
2023-03-08 16:55:15 -0300

    teste funciona

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
+global.ResizeObserver = require('resize-observer-polyfill')
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('teste do titulo', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const testCase = document.querySelector('.App h1');
+  expect(testCase).toHaveTextContent(/meu painel de controle/i);
 });
