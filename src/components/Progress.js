8461b9981d887c12d31e2be9a6faa330ed8f25c1
Beatriz <beatrizleite_aragao@hotmail.com>
2023-03-08 15:41:17 -0300

    mudança de pasta

---
+++ src/components/Progress.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import Stack from '@mui/material/Stack';
+import CircularProgress from '@mui/material/CircularProgress';
+
+export default function Circular() {
+  return (
+    
+    
+      <CircularProgress color="success" /> 
+
+  );
+}
